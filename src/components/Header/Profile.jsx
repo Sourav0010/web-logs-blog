16afb7aec784b24929f484337c17065cdbaa6879
Sourav0010 <mohantysourav656@gmail.com>
2024-07-27 21:34:59 +0530

    profile card updated

--- src/components/Header/Profile.jsx
+++ src/components/Header/Profile.jsx
@@ -45,7 +45,9 @@ function Profile() {
             }} class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                 <span class="sr-only">Open user menu</span>
                 <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+            
+                <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="https://avatar.iran.liara.run/public/boy" alt="Bordered avatar"/>
+
         </div>
             </button>
             
