c9da2238183a60f73ab71855fc6a0fb86bee3f10
Sourav0010 <mohantysourav656@gmail.com>
2024-07-31 21:29:12 +0530

    minor bug create post fixed

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,10 +2,13 @@ import React,{useEffect} from 'react'
 import {PostCard,Button, Loading} from './index'
 import appwriteDatabase from '../appwrite/appwriteDatabase.js'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 function Articles() {
   let [posts, setPosts] = React.useState([]);
   let [loading, setLoading] = React.useState(false);
   let [page, setPage] = React.useState(0);
+  const user = useSelector(state => state.userAuth.userData.userData)
 
   useEffect(()=>{
     setLoading(true);
@@ -18,13 +21,13 @@ function Articles() {
   
   return (
     <>
-      <div className='flex flex-row items-center justify-end px-10'>
+     {user?.labels[0] === 'admin' &&  <div className='flex flex-row items-center justify-end px-10'>
         <Link to='/test'>
            <Button className='hidden max-sm:block  max-sm:text-xs border border-black  text-black px-4 py-2 ' >
             <i class="fa-solid fa-plus"></i> Create
             </Button>
         </Link>
-      </div>
+      </div>}
 
       <div className=' flex items-center gap-2 my-20 flex-wrap justify-center flex-row'>
         {loading ? (
@@ -47,11 +50,12 @@ function Articles() {
         })}
       </div>
 
-      <div className={`flex justify-center gap-2 my-4`} >
+      <div className={`flex justify-center items-center gap-2 my-4`} >
         <Button onClick={()=>{setPage( page => page-1)}} disabled={page==0} 
         className={` text-white bg-black rounded-md max-sm:text-xs max-sm:px-3 max-sm:py-2 px-4 py-2  ${page==0 && 'bg-gray-700'}`} >
           Prev Post
         </Button>
+        {` Page ${page+1} `}
         <Button onClick={()=>{setPage(page => page+1)}} disabled={!posts.length} 
         className={` text-white bg-black rounded-md max-sm:text-xs max-sm:px-3 max-sm:py-2 px-4 py-2 ${!posts.length && 'bg-gray-700'}`} >
           Next Post
