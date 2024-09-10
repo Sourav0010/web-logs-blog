50635d87b4561c54a7d4242347757cd13d16edcb
Sourav0010 <mohantysourav656@gmail.com>
2024-09-10 11:08:34 +0530

    fixed tinymce editor premium issue

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -14,7 +14,7 @@ function TextEditor({name , control, defaultValue = ''}) {
             apiKey={conf.tinyKey}
             initialValue={defaultValue}
             init={{
-            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate  mentions  tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount  linkchecker',
             menubar: false,
             toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
           }}
